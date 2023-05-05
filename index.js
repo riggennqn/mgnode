8f06337491a82f114a382577713f7f8f958885a2
andresucho pucho <andresuchopuchopichi@gmail.com>
2023-05-05 12:54:29 -0300

    ahora si

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const http = require('http');
 
 function requestController() {
 
-    document.write('hola mundo');
+    //document.write('hola mundo');
+    console.log('probando el funcioamiento');
+    
     
 }
 const server =http.createServer(requestController);
